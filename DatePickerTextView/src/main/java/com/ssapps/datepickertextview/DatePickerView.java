0b1f26514dfb9dd4801f07a490bf638dd27ba80e
sagar <ssapps.sagar@gmail.com>
2024-12-09 14:04:39 +0530

    Initial commit

---
+++ DatePickerTextView/src/main/java/com/ssapps/datepickertextview/DatePickerView.java
@@ -0,0 +1,82 @@
+package com.ssapps.datepickertextview;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.ssapps.datepickertextview.databinding.DatePickerViewBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DatePickerView extends LinearLayout {
+    private final @NonNull DatePickerViewBinding binding;
+
+    private SimpleDateFormat dateFormat;
+
+    public DatePickerView(Context context, @Nullable AttributeSet attrs){
+        super(context, attrs);
+
+        binding = DatePickerViewBinding.inflate(LayoutInflater.from(context), this, true);
+
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.DatePickerView, 0, 0);
+        try {
+            int iconResId = attributes.getResourceId(R.styleable.DatePickerView_icon, -1);
+            if (iconResId != -1){
+                binding.datePickerActions.setImageResource(iconResId);
+            }
+
+            String format = attributes.getString(R.styleable.DatePickerView_dateFormat);
+            setDateFormat(format==null?"dd/MM/yyyy":format);
+        } finally {
+            attributes.recycle();
+        }
+        Calendar calendar = Calendar.getInstance();
+
+        binding.dateText.setText(dateFormat.format(calendar.getTime()));
+
+        binding.datePickerActions.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog.OnDateSetListener listener  = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        calendar.set(year, month, dayOfMonth);
+                        binding.dateText.setText(dateFormat.format(calendar.getTime()));
+                    }
+                };
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(context, listener,
+                        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONDAY), calendar.get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.show();
+            }
+        });
+    }
+
+    public void setDateFormat(String format){
+        dateFormat = new SimpleDateFormat(format, Locale.getDefault());
+    }
+
+    public void setIcon(Drawable drawable){
+        binding.datePickerActions.setImageDrawable(drawable);
+    }
+
+    public String getSelectedDate() {
+        return binding.dateText.getText().toString();
+    }
+}
+
---
+++ app/src/main/java/com/ssapps/dptv/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ssapps.dptv;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ssapps.dptv.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
